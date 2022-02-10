fee003d1d6a2f45c221514ef0f7dfa2688e0711e
IamGab <gabrielarafolmendoza25@yahoo.com>
2022-02-10 17:27:51 +0800

    Add index in service

--- app/Http/Controllers/serviceController.php
+++ app/Http/Controllers/serviceController.php
@@ -6,3 +6,16 @@ use Illuminate\Http\Request;
 use App\Models\Service;
 use Illuminate\Support\Facades\File;
 
+class serviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $services = Service::all();
+        return view('service.index',['services' => $services]);
+    }
+
